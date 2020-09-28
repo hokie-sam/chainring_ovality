3c053312781adb1db8637c05e56f09c39b4ea746
hokie-sam <samfelder90@gmail.com>
2020-09-28 18:06:24 -0400

    some small spelling changes

--- ovality_calc.py
+++ ovality_calc.py
@@ -10,11 +10,11 @@ right = 2 - 2 * ovality + ovality**2
 a = math.sqrt(left/right)
 b = a - ovality*a
 
-round_radius = perimeter/math.pi/2
+circle_radius = perimeter/math.pi/2
 
-print(f"\nLongest radius (a) is {round(a,2)}")
+print(f"\nLongest radius (a) is {round(a,2)} (chainring teeth)")
 print(f"This feels like using a {round(a*2*math.pi,1)} tooth chainring\n")
-print(f"Shortest radius (b) is {round(b,2)}")
+print(f"Shortest radius (b) is {round(b,2)} (chainring teeth)")
 print(f"This feels like using a {round(b*2*math.pi,1)} tooth chainring\n")
-print(f"A circular chainring with {int(perimeter)} teeth would have a radius of {round(round_radius,2)}")
+print(f"A circular chainring with {int(perimeter)} teeth would have a radius of {round(circle_radius,2)}")
 input()
